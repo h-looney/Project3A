a95f616620379233c6cb2c4f33e57c2d804abf91
lily-teuscher <47393990+lily-teuscher@users.noreply.github.com>
2022-11-10 17:04:02 -0600

    Update forms.py

--- flask_wtforms_tutorial/forms.py
+++ flask_wtforms_tutorial/forms.py
@@ -1,4 +1,7 @@
 """Form class declaration."""
+import csv
+import requests 
+
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import (
     DateField,
@@ -15,12 +18,21 @@ from wtforms.validators import URL, DataRequired, Email, EqualTo, Length
 class StockForm(FlaskForm):
     """Generate Your Graph."""
     
+    CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=AU4UXQO6WFYENR4J'
+
+    choices = []
+
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        for row in my_list:
+            choices.append((row[0], row[0]))
+    
     #THIS IS WHERE YOU WILL IMPLEMENT CODE TO POPULATE THE SYMBOL FIELD WITH STOCK OPTIONS
     symbol = SelectField("Choose Stock Symbol",[DataRequired()],
-        choices=[
-            ("IBM", "IBM"),
-            ("GOOGL", "GOOGL"),
-        ],
+         choices=choices,
     )
 
     chart_type = SelectField("Select Chart Type",[DataRequired()],
